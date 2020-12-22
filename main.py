6b8235877496d71ce5fad85eb007186dc28985d1
Imtiaz A <ahmedimtiaz.ahmed@gmail.com>
2020-12-22 02:09:50 +0000

    .

--- main.py
+++ main.py
@@ -8,14 +8,14 @@ secret_password = '*' * password_length
 print(f"{user_name}  is your username and {secret_password} is your password which is {password_length} characters long")
 
 def remove_even(list):
-    # Write your code here!
+    
 
     for item in list:
 		if item%2 = 0:
         	print(item)
-        #
-       # elif item % 2 == 0:
-        #    return item.pop(item)
+        
+        elif item % 2 == 0:
+           return item.pop(item)
 remove_even([1,2,4])
 
 def remove_even(list):
@@ -33,3 +33,10 @@ elif 10 <= value <= 20:
     print("I'd still pay that...")
 else:
     print("Wow! That's too much!")
+
+''' List Comprehension '''
+
+
+string = ['1', '2', '3', '4', '5']
+integer = [int(element) for element in string]
+print(integer)
